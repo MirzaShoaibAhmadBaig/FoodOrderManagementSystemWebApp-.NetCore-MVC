9355fa35ed08975bad356cb6b295481a5b171969
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-26 12:24:20 +0500

    Add DB Contaxt and Genric Interface Implimentation

---
+++ FoodOrder.DataAccess/Infrasturcture/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.DataAccess.Infrasturcture
+{
+  public  interface  IGenericRepository<T> where T :class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int Id);
+        void Insert(T enetity);
+        void Delete(T enetity);
+
+        void DeleteRange(IEnumerable<T> enetitie);
+    }
+}
---
+++ FoodOrder.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow.AddHours(5);
+
+
+    }
+}
